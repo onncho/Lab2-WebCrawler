61b169330b30f1d352b6c3c173836d6308cb0ae8
onncho <onncho@outlook.com>
2016-02-12 23:51:58 +0200

    web server

--- src/WebServer.java
+++ src/WebServer.java
@@ -3,7 +3,8 @@ import java.util.HashMap;
 
 public class WebServer {
 	
-	public static final boolean TEST_PORT_SCAN = true;
+	public static final boolean TEST_PORT_SCAN = false;
+	public static int Number_Of_Downloaders = 0;
 	
 	public static void main(String[] args) throws IOException
 	{
@@ -21,41 +22,45 @@ public class WebServer {
 		//ServerListener webserver = new ServerListener(threadPool);
 
 		//webserver.start();
-		
-		//DownloaderThreadPool threadP = new DownloaderThreadPool(10);
+		Number_Of_Downloaders = Integer.parseInt(ConfigurationObject.getMaxDownloaders());
+		//DownloaderThreadPool threadP = new DownloaderThreadPool(Number_Of_Downloaders);
 		//ServerListener webSrv = new ServerListener(threadP);
-		
 		//webSrv.start();
+		
 		CrawlerControler.getInstance();
+		
 		//if input port scan
 		if (TEST_PORT_SCAN) {
-			
-			//TODO - make it another method
-			Thread[] scanners = new Thread[10];
-			int startScanPort = 0;
-			int portsPerScanner = 100;  // TODO - change it
-			PortScannerLatch scannerLatch = PortScannerLatch.getInstance();
-			
-			//Create threads
-			for (int i = 0; i < scanners.length; i++) {
-				int endPort = startScanPort + portsPerScanner;
-				PortScanner scanner = new PortScanner("http://smallbasic.com",
-						startScanPort, endPort, scannerLatch);
-				scanners[i] = new Thread(scanner);
-				startScanPort = endPort + 1;
-			}
-			
-			//Start thread
-			for (Thread thread : scanners) {
-				thread.start();
-			}
-			
-		} else {
-			startCrawling();
-		}
+			startPortScanner();
+		} 
+		// TODO: check how to get the indicatation to start the web crawler and to The Port Sccanner
+		startCrawling();
 			
 	}
 	
+	private static void startPortScanner() {
+		Thread[] scanners = new Thread[Number_Of_Downloaders];
+		int startScanPort = 0;
+		int portsPerScanner = 65535 / Number_Of_Downloaders;
+		
+		PortScannerLatch scannerLatch = PortScannerLatch.getInstance();
+		
+		//Create threads
+		for (int i = 0; i < scanners.length; i++) {
+			int endPort = startScanPort + portsPerScanner;
+			PortScanner scanner = new PortScanner("http://smallbasic.com",
+					startScanPort, endPort, scannerLatch);
+			scanners[i] = new Thread(scanner);
+			startScanPort = endPort + 1;
+		}
+		
+		//Start thread
+		for (Thread thread : scanners) {
+			thread.start();
+		}
+	}
+	
+	// get the input from the user of the requested domain
 	public static void startCrawling() {
 		DownloaderTask task = new DownloaderTask("http://smallbasic.com");
 		CrawlerControler.getInstance().addTaskToDownloaderQueue(task);
